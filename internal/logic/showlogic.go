09ae8a58d6aa5d8313ca2c6937595aed81564f65
ranguangdeng <ranguandeng@gmail.com>
2023-07-19 10:27:57 +0800

    完成查看短链功能

--- internal/logic/showlogic.go
+++ internal/logic/showlogic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 
 	"shortener/internal/svc"
 	"shortener/internal/types"
@@ -24,7 +26,17 @@ func NewShowLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ShowLogic {
 }
 
 func (l *ShowLogic) Show(req *types.ShowRequest) (resp *types.ShowResponse, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	// 输入短链接，重定向到真实的链接
+	// 1. 根据短链接查找原始的长链接
+	u,err:=l.svcCtx.ShortUrlModel.FindOneBySurl(l.ctx,sql.NullString{Valid:true,String:req.ShortUrl})
+	if err!=nil{
+		if err==sql.ErrNoRows{
+			return nil,errors.New("404")
+		}
+		logx.Errorw("ShortUrlModel.FindOneBySurl failed",logx.LogField{Value:err.Error(),Key:"err"})
+		return nil,err
+	}
+	// 2. 返回重定向响应/返回查找到的长链接，在调用handler层返回重定向响应
+	
+	return &types.ShowResponse{LongUrl: u.Lurl.String},nil
 }
